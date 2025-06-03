3a04a12884be507411baba88190c5e0b57eae650
Swllen <2441312713@qq.com>
2025-06-03 13:06:20 +0800

    repair name error

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ USERVO_BAUDRATE = 115200  # Baud rate for the servo manager
 USERVO_TIMEOUT = 1  # Timeout for serial communication
 
 CAR_PORT = '/dev/ttyACM0'  # Serial port for the car control
-CAR_BOUDRATE = 230400  # Baud rate for the car control
+CAR_BAUDRATE = 230400  # Baud rate for the car control
 PASSWORD = 'stxin.789.'  # Password for granting permission to access the serial port
 
-DEBUG = True  # Debug mode for additional logging
+DEBUG = False  # Debug mode for additional logging
 LOG_DIR = './Logger/logs'  # Directory for log files
 
 IMAGE_QUEUE_SIZE = 10  # Size of the image queue for camera frames
@@ -19,4 +19,4 @@ REMOTE_IMAGE_QUEUE=queue.Queue(maxsize=IMAGE_QUEUE_SIZE)
 GET_TARGET = False
 WIN_SIGNAL = False
 
-NET_PATH = '/home/swllen/yolo/Car/yolov5-v7.0'
+NET_PATH = 'yolov5/best.engine'
